324095dc4d1b085057473398470cb88c337565d6
Alexey Pakseykin <uvsmtid@gmail.com>
2025-10-19 23:42:30 +0800

    Reduce min required `python` version to 3.7

--- src/protoprimer/test/test_protoprimer/test_fast_mocked/test_func_simple/test_ensure_min_python_version.py
+++ src/protoprimer/test/test_protoprimer/test_fast_mocked/test_func_simple/test_ensure_min_python_version.py
@@ -15,12 +15,12 @@ def test_relationship():
 
 class ThisTestClass(BaseTestClass):
 
-    @patch("sys.version_info", (3, 8, 0))
+    @patch("sys.version_info", (3, 7, 0))
     def test_version_succeeds(self):
         # FT_84_11_73_28: supported python versions:
         ensure_min_python_version()
 
-    @patch("sys.version_info", (3, 7, 5))
+    @patch("sys.version_info", (3, 6, 5))
     def test_version_fails(self):
         # FT_84_11_73_28: supported python versions:
         with self.assertRaises(AssertionError) as context:
