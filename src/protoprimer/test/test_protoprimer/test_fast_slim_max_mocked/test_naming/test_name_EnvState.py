36775f3a9cd5c986ff9e36d19ab48290135453e0
Alexey Pakseykin <uvsmtid@gmail.com>
2026-03-28 23:58:02 +0800

    complete_setup

--- src/protoprimer/test/test_protoprimer/test_fast_slim_max_mocked/test_naming/test_name_EnvState.py
+++ src/protoprimer/test/test_protoprimer/test_fast_slim_max_mocked/test_naming/test_name_EnvState.py
@@ -2,17 +2,26 @@ from __future__ import annotations
 
 import enum
 
-from local_test.consistent_naming import verify_name_enum_order_in_name
 from local_test.name_assertion import (
-    assert_test_func_name_embeds_str,
     assert_test_module_name_embeds_str,
 )
 from protoprimer.primer_kernel import (
+    CommandAction,
+    ConfConstGeneral,
+    ConfLeap,
     EnvState,
+    FilesystemObject,
+    KeyWord,
+    PathName,
+    PathType,
+    StateStride,
+    ValueName,
 )
 from test_protoprimer.test_fast_slim_max_mocked.test_naming.naming_metadata import (
     AbstractMeta,
+    CompletedAction,
     NameCategory,
+    ValueSource,
 )
 from test_protoprimer.test_fast_slim_max_mocked.test_naming.naming_test_base import (
     NamingTestBase,
@@ -25,10 +34,12 @@ class StateMeta(AbstractMeta):
         self,
         env_state: EnvState,
         name_category: NameCategory,
+        name_components: list[str],
     ):
         # Specified at ctor call site (primarily, for quick navigation in the IDE):
         self.env_state: EnvState = env_state
         self.name_category: NameCategory = name_category
+        self.name_components: list[str] = name_components
 
     def get_prod_item(self):
         return self.env_state
@@ -42,6 +53,9 @@ class StateMeta(AbstractMeta):
     def get_category(self):
         return self.name_category
 
+    def get_name_components(self) -> list[str]:
+        return self.name_components
+
 
 class StateName(enum.Enum):
     """
@@ -51,231 +65,530 @@ class StateName(enum.Enum):
     state_input_py_exec_var_loaded = StateMeta(
         EnvState.state_input_py_exec_var_loaded,
         NameCategory.category_named_value,
+        [
+            KeyWord.key_state.value,
+            ConfLeap.leap_input.value,
+            ValueName.value_py_exec.value,
+            ValueSource.value_var.value,
+            CompletedAction.action_loaded.value,
+        ],
     )
 
     state_input_stderr_log_level_var_loaded = StateMeta(
         EnvState.state_input_stderr_log_level_var_loaded,
         NameCategory.category_named_value,
+        [
+            KeyWord.key_state.value,
+            ConfLeap.leap_input.value,
+            ValueName.value_stderr_log_level.value,
+            ValueSource.value_var.value,
+            CompletedAction.action_loaded.value,
+        ],
     )
 
     state_input_do_install_var_loaded = StateMeta(
         EnvState.state_input_do_install_var_loaded,
         NameCategory.category_named_value,
+        [
+            KeyWord.key_state.value,
+            ConfLeap.leap_input.value,
+            ValueName.value_do_install.value,
+            ValueSource.value_var.value,
+            CompletedAction.action_loaded.value,
+        ],
     )
 
     state_default_stderr_log_handler_configured = StateMeta(
         EnvState.state_default_stderr_log_handler_configured,
         NameCategory.category_state_mutation,
+        [
+            KeyWord.key_state.value,
+            KeyWord.key_default.value,
+            KeyWord.key_stderr.value,
+            KeyWord.key_log.value,
+            KeyWord.key_handler.value,
+            KeyWord.key_configured.value,
+        ],
     )
 
     state_default_file_log_handler_configured = StateMeta(
         EnvState.state_default_file_log_handler_configured,
         NameCategory.category_state_mutation,
+        [
+            KeyWord.key_state.value,
+            KeyWord.key_default.value,
+            FilesystemObject.fs_object_file.value,
+            KeyWord.key_log.value,
+            KeyWord.key_handler.value,
+            KeyWord.key_configured.value,
+        ],
     )
 
     state_args_parsed = StateMeta(
         EnvState.state_args_parsed,
         NameCategory.category_name_only,
+        [
+            KeyWord.key_state.value,
+            KeyWord.key_args.value,
+            KeyWord.key_parsed.value,
+        ],
     )
 
     state_input_stderr_log_level_eval_finalized = StateMeta(
         EnvState.state_input_stderr_log_level_eval_finalized,
         NameCategory.category_named_value,
+        [
+            KeyWord.key_state.value,
+            ConfLeap.leap_input.value,
+            ValueName.value_stderr_log_level.value,
+            ValueSource.value_eval.value,
+            CompletedAction.action_finalized.value,
+        ],
     )
 
     state_input_run_mode_arg_loaded = StateMeta(
         EnvState.state_input_run_mode_arg_loaded,
         NameCategory.category_named_value,
+        [
+            KeyWord.key_state.value,
+            ConfLeap.leap_input.value,
+            ValueName.value_run_mode.value,
+            ValueSource.value_arg.value,
+            CompletedAction.action_loaded.value,
+        ],
     )
 
     state_input_final_state_eval_finalized = StateMeta(
         EnvState.state_input_final_state_eval_finalized,
         NameCategory.category_named_value,
+        [
+            KeyWord.key_state.value,
+            ConfLeap.leap_input.value,
+            ValueName.value_final_state.value,
+            ValueSource.value_eval.value,
+            CompletedAction.action_finalized.value,
+        ],
     )
 
     state_run_mode_executed = StateMeta(
         EnvState.state_run_mode_executed,
         NameCategory.category_state_mutation,
+        [
+            KeyWord.key_state.value,
+            KeyWord.key_run.value,
+            KeyWord.key_mode.value,
+            KeyWord.key_executed.value,
+        ],
     )
 
     state_input_start_id_var_loaded = StateMeta(
         EnvState.state_input_start_id_var_loaded,
         NameCategory.category_named_value,
+        [
+            KeyWord.key_state.value,
+            ConfLeap.leap_input.value,
+            ValueName.value_start_id.value,
+            ValueSource.value_var.value,
+            CompletedAction.action_loaded.value,
+        ],
     )
 
     state_input_proto_code_file_abs_path_var_loaded = StateMeta(
         EnvState.state_input_proto_code_file_abs_path_var_loaded,
         NameCategory.category_path_value_deprecated,
+        [
+            KeyWord.key_state.value,
+            ConfLeap.leap_input.value,
+            PathName.path_proto_code.value,
+            FilesystemObject.fs_object_file.value,
+            PathType.path_abs.value,
+            ValueSource.value_var.value,
+            CompletedAction.action_loaded.value,
+        ],
     )
 
     state_stride_py_arbitrary_reached = StateMeta(
         EnvState.state_stride_py_arbitrary_reached,
         NameCategory.category_python_exec,
+        [
+            KeyWord.key_state.value,
+            StateStride.stride_py_arbitrary.name,
+            KeyWord.key_reached.value,
+        ],
     )
 
     state_proto_code_file_abs_path_inited = StateMeta(
         EnvState.state_proto_code_file_abs_path_inited,
         NameCategory.category_path_value,
+        [
+            KeyWord.key_state.value,
+            PathName.path_proto_code.value,
+            FilesystemObject.fs_object_file.value,
+            PathType.path_abs.value,
+            CompletedAction.action_inited.value,
+        ],
     )
 
     state_primer_conf_file_abs_path_inited = StateMeta(
         EnvState.state_primer_conf_file_abs_path_inited,
         NameCategory.category_path_value,
+        [
+            KeyWord.key_state.value,
+            PathName.path_primer_conf.value,
+            FilesystemObject.fs_object_file.value,
+            PathType.path_abs.value,
+            CompletedAction.action_inited.value,
+        ],
     )
 
     state_primer_conf_file_data_loaded = StateMeta(
         EnvState.state_primer_conf_file_data_loaded,
         NameCategory.category_loaded_data,
+        [
+            KeyWord.key_state.value,
+            ConfLeap.leap_primer.value,
+            KeyWord.key_conf.value,
+            FilesystemObject.fs_object_file.value,
+            KeyWord.key_data.value,
+            CompletedAction.action_loaded.value,
+        ],
     )
 
     state_ref_root_dir_abs_path_inited = StateMeta(
         EnvState.state_ref_root_dir_abs_path_inited,
         NameCategory.category_path_value,
+        [
+            KeyWord.key_state.value,
+            PathName.path_ref_root.value,
+            FilesystemObject.fs_object_dir.value,
+            PathType.path_abs.value,
+            CompletedAction.action_inited.value,
+        ],
     )
 
     state_global_conf_dir_abs_path_inited = StateMeta(
         EnvState.state_global_conf_dir_abs_path_inited,
         NameCategory.category_path_value,
+        [
+            KeyWord.key_state.value,
+            PathName.path_global_conf.value,
+            FilesystemObject.fs_object_dir.value,
+            PathType.path_abs.value,
+            CompletedAction.action_inited.value,
+        ],
     )
 
     state_global_conf_file_abs_path_inited = StateMeta(
         EnvState.state_global_conf_file_abs_path_inited,
         NameCategory.category_path_value,
+        [
+            KeyWord.key_state.value,
+            PathName.path_global_conf.value,
+            FilesystemObject.fs_object_file.value,
+            PathType.path_abs.value,
+            CompletedAction.action_inited.value,
+        ],
     )
 
     state_client_conf_file_data_loaded = StateMeta(
         EnvState.state_client_conf_file_data_loaded,
         NameCategory.category_loaded_data,
+        [
+            KeyWord.key_state.value,
+            ConfLeap.leap_client.value,
+            KeyWord.key_conf.value,
+            FilesystemObject.fs_object_file.value,
+            KeyWord.key_data.value,
+            CompletedAction.action_loaded.value,
+        ],
     )
 
     state_local_conf_symlink_abs_path_inited = StateMeta(
         EnvState.state_local_conf_symlink_abs_path_inited,
         NameCategory.category_path_value,
+        [
+            KeyWord.key_state.value,
+            PathName.path_local_conf.value,
+            FilesystemObject.fs_object_symlink.value,
+            PathType.path_abs.value,
+            CompletedAction.action_inited.value,
+        ],
     )
 
     state_selected_env_dir_rel_path_inited = StateMeta(
         EnvState.state_selected_env_dir_rel_path_inited,
         NameCategory.category_path_value,
+        [
+            KeyWord.key_state.value,
+            PathName.path_selected_env.value,
+            FilesystemObject.fs_object_dir.value,
+            PathType.path_rel.value,
+            CompletedAction.action_inited.value,
+        ],
     )
 
     state_local_conf_file_abs_path_inited = StateMeta(
         EnvState.state_local_conf_file_abs_path_inited,
         NameCategory.category_path_value,
+        [
+            KeyWord.key_state.value,
+            PathName.path_local_conf.value,
+            FilesystemObject.fs_object_file.value,
+            PathType.path_abs.value,
+            CompletedAction.action_inited.value,
+        ],
     )
 
     state_env_conf_file_data_loaded = StateMeta(
         EnvState.state_env_conf_file_data_loaded,
         NameCategory.category_loaded_data,
+        [
+            KeyWord.key_state.value,
+            ConfLeap.leap_env.value,
+            KeyWord.key_conf.value,
+            FilesystemObject.fs_object_file.value,
+            KeyWord.key_data.value,
+            CompletedAction.action_loaded.value,
+        ],
     )
 
     state_required_python_version_inited = StateMeta(
         EnvState.state_required_python_version_inited,
         NameCategory.category_value_field_action,
+        [
+            KeyWord.key_state.value,
+            PathName.path_required_python.value,
+            ValueName.value_version.value,
+            CompletedAction.action_inited.value,
+        ],
     )
 
     state_python_selector_file_abs_path_inited = StateMeta(
         EnvState.state_python_selector_file_abs_path_inited,
         NameCategory.category_path_value,
+        [
+            KeyWord.key_state.value,
+            PathName.path_python_selector.value,
+            FilesystemObject.fs_object_file.value,
+            PathType.path_abs.value,
+            CompletedAction.action_inited.value,
+        ],
     )
 
     state_selected_python_file_abs_path_inited = StateMeta(
         EnvState.state_selected_python_file_abs_path_inited,
         NameCategory.category_path_value,
+        [
+            KeyWord.key_state.value,
+            PathName.path_selected_python.value,
+            FilesystemObject.fs_object_file.value,
+            PathType.path_abs.value,
+            CompletedAction.action_inited.value,
+        ],
     )
 
     state_local_venv_dir_abs_path_inited = StateMeta(
         EnvState.state_local_venv_dir_abs_path_inited,
         NameCategory.category_path_value,
+        [
+            KeyWord.key_state.value,
+            PathName.path_local_venv.value,
+            FilesystemObject.fs_object_dir.value,
+            PathType.path_abs.value,
+            CompletedAction.action_inited.value,
+        ],
     )
 
     state_local_log_dir_abs_path_inited = StateMeta(
         EnvState.state_local_log_dir_abs_path_inited,
         NameCategory.category_path_value,
+        [
+            KeyWord.key_state.value,
+            PathName.path_local_log.value,
+            FilesystemObject.fs_object_dir.value,
+            PathType.path_abs.value,
+            CompletedAction.action_inited.value,
+        ],
     )
 
     state_local_tmp_dir_abs_path_inited = StateMeta(
         EnvState.state_local_tmp_dir_abs_path_inited,
         NameCategory.category_path_value,
+        [
+            KeyWord.key_state.value,
+            PathName.path_local_tmp.value,
+            FilesystemObject.fs_object_dir.value,
+            PathType.path_abs.value,
+            CompletedAction.action_inited.value,
+        ],
     )
 
     state_local_cache_dir_abs_path_inited = StateMeta(
         EnvState.state_local_cache_dir_abs_path_inited,
         NameCategory.category_path_value,
+        [
+            KeyWord.key_state.value,
+            PathName.path_local_cache.value,
+            FilesystemObject.fs_object_dir.value,
+            PathType.path_abs.value,
+            CompletedAction.action_inited.value,
+        ],
     )
 
     state_project_descriptors_inited = StateMeta(
         EnvState.state_project_descriptors_inited,
         NameCategory.category_value_field_action,
+        [
+            KeyWord.key_state.value,
+            ValueName.value_project_descriptors.value,
+            CompletedAction.action_inited.value,
+        ],
     )
 
     state_install_specs_inited = StateMeta(
         EnvState.state_install_specs_inited,
         NameCategory.category_value_field_action,
+        [
+            KeyWord.key_state.value,
+            ValueName.value_install_specs.value,
+            CompletedAction.action_inited.value,
+        ],
     )
 
     state_venv_driver_inited = StateMeta(
         EnvState.state_venv_driver_inited,
         NameCategory.category_value_field_action,
+        [
+            KeyWord.key_state.value,
+            ValueName.value_venv_driver.value,
+            CompletedAction.action_inited.value,
+        ],
     )
 
     state_derived_conf_data_loaded = StateMeta(
         EnvState.state_derived_conf_data_loaded,
         NameCategory.category_state_mutation,
+        [
+            KeyWord.key_state.value,
+            KeyWord.key_derived.value,
+            KeyWord.key_conf.value,
+            KeyWord.key_data.value,
+            CompletedAction.action_loaded.value,
+        ],
     )
 
     state_effective_config_data_printed = StateMeta(
         EnvState.state_effective_config_data_printed,
         NameCategory.category_state_mutation,
+        [
+            KeyWord.key_state.value,
+            KeyWord.key_effective.value,
+            KeyWord.key_config.value,
+            KeyWord.key_data.value,
+            KeyWord.key_printed.value,
+        ],
     )
 
     state_stride_py_required_reached = StateMeta(
         EnvState.state_stride_py_required_reached,
         NameCategory.category_python_exec,
+        [
+            KeyWord.key_state.value,
+            StateStride.stride_py_required.name,
+            KeyWord.key_reached.value,
+        ],
     )
 
     state_reinstall_triggered = StateMeta(
         EnvState.state_reinstall_triggered,
         NameCategory.category_state_mutation,
+        [
+            KeyWord.key_state.value,
+            CommandAction.action_reinstall.value,
+            KeyWord.key_triggered.value,
+        ],
     )
 
     state_stride_py_venv_reached = StateMeta(
         EnvState.state_stride_py_venv_reached,
         NameCategory.category_python_exec,
+        [
+            KeyWord.key_state.value,
+            StateStride.stride_py_venv.name,
+            KeyWord.key_reached.value,
+        ],
     )
 
     state_protoprimer_package_installed = StateMeta(
         EnvState.state_protoprimer_package_installed,
         NameCategory.category_state_mutation,
+        [
+            KeyWord.key_state.value,
+            ConfConstGeneral.name_protoprimer_package,
+            KeyWord.key_package.value,
+            KeyWord.key_installed.value,
+        ],
     )
 
     state_version_constraints_generated = StateMeta(
         EnvState.state_version_constraints_generated,
         NameCategory.category_state_mutation,
+        [
+            KeyWord.key_state.value,
+            ValueName.value_version.value,
+            KeyWord.key_constraints.value,
+            KeyWord.key_generated.value,
+        ],
     )
 
     state_stride_deps_updated_reached = StateMeta(
         EnvState.state_stride_deps_updated_reached,
         NameCategory.category_python_exec,
+        [
+            KeyWord.key_state.value,
+            StateStride.stride_deps_updated.name,
+            KeyWord.key_reached.value,
+        ],
     )
 
     state_proto_code_updated = StateMeta(
         EnvState.state_proto_code_updated,
         NameCategory.category_state_mutation,
+        [
+            KeyWord.key_state.value,
+            PathName.path_proto_code.value,
+            KeyWord.key_updated.value,
+        ],
     )
 
     state_stride_src_updated_reached = StateMeta(
         EnvState.state_stride_src_updated_reached,
         NameCategory.category_python_exec,
+        [
+            KeyWord.key_state.value,
+            StateStride.stride_src_updated.name,
+            KeyWord.key_reached.value,
+        ],
     )
 
     state_command_executed = StateMeta(
         EnvState.state_command_executed,
         NameCategory.category_state_mutation,
+        [
+            KeyWord.key_state.value,
+            CommandAction.action_command.value,
+            KeyWord.key_executed.value,
+        ],
     )
 
     state_venv_driver_prepared = StateMeta(
         EnvState.state_venv_driver_prepared,
         NameCategory.category_state_mutation,
+        [
+            KeyWord.key_state.value,
+            ValueName.value_venv_driver.value,
+            KeyWord.key_prepared.value,
+        ],
     )
 
 
@@ -288,18 +601,3 @@ class TestStateName(NamingTestBase):
         assert_test_module_name_embeds_str(
             self.prod_enum.__name__,
         )
-
-    def test_state_selected_env_dir_rel_path_inited(self):
-        """
-        A sample for debugging.
-        """
-        assert_test_func_name_embeds_str(
-            self.test_enum.state_selected_env_dir_rel_path_inited.name,
-        )
-
-        ret_val: enum.Enum | None = verify_name_enum_order_in_name(
-            self.test_enum.state_selected_env_dir_rel_path_inited.value.get_category().value.name_enums,
-            self.test_enum.state_selected_env_dir_rel_path_inited.name,
-        )
-
-        assert ret_val is None
