0b83a589e1ee69b81108d1ecb13994d04d7f1e51
uvsmtid <uvsmtid@gmail.com>
2026-09-06 21:34:12 +0700

    Add "Manual" doc with `sybil`-executed examples (#125)

---
+++ doc/conftest.py
@@ -0,0 +1,41 @@
+import subprocess
+
+from sybil import Sybil
+from sybil.parsers.myst import CodeBlockParser, PythonCodeBlockParser, SkipParser
+
+
+def evaluate_shell(code_example):
+    """
+    Executes `shell` code blocks embedded in the specified docs as `pytest` tests.
+    """
+    __tracebackhide__ = True
+    completed_process = subprocess.run(
+        code_example.parsed,
+        shell=True,
+        cwd=code_example.namespace["repo_dir"],
+        capture_output=True,
+        text=True,
+    )
+    if completed_process.returncode != 0:
+        return "\n".join(
+            [
+                f"code example: {code_example.parsed}",
+                f"exit code: {completed_process.returncode}",
+                "stdout:",
+                completed_process.stdout,
+                "stderr:",
+                completed_process.stderr,
+            ]
+        )
+
+
+pytest_collect_file = Sybil(
+    parsers=[
+        PythonCodeBlockParser(),
+        CodeBlockParser(language="shell", evaluator=evaluate_shell),
+        SkipParser(),
+    ],
+    patterns=[
+        "manual.md",
+    ],
+).pytest()
